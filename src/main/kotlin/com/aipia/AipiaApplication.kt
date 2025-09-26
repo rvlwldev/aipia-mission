94258c3ba4265b6ccb8996b1586209ef8fc03d3e
rvlwldev <rvlwlasd77@gmail.com>
2025-09-26 12:11:54 +0900

    config: JPA 감사설정 (EnableJpaAuditing) 추가

--- src/main/kotlin/com/aipia/AipiaApplication.kt
+++ src/main/kotlin/com/aipia/AipiaApplication.kt
@@ -3,11 +3,13 @@ package com.aipia
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
+@EnableJpaAuditing
 class AipiaApplication
 
 fun main(args: Array<String>) {
